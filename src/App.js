672d38953fc80db525401e0be4dc1466114121c5
Juanma Vega <juanmavega83@gmail.com>
2022-03-17 12:47:57 +0100

    creating routes, navBar, links and, installing firebase for authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/homepage.component';
+import Navigation from './routes/navigation/navigation.component'
 
-const App = () => (
-    <div className="App">
-      <Homepage />
+const Shop = () => {
+  return(
+    <div>
+      <h1>SHOP COMPONENT</h1>
     </div>
+  )
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
+};
 
 export default App;
